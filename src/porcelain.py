fb2a8d21441cb03ef0039c4356c0303b3bf34457
Bram <174822895+ItayCohen-Prog@users.noreply.github.com>
2026-06-12 01:03:39 +0300

    added some error handling

--- src/porcelain.py
+++ src/porcelain.py
@@ -7,7 +7,12 @@ app = typer.Typer()
 
 @app.command()
 def init():
-    ntry_dir = NtryFilesys()
-    ntry_dir.create_empty_filesystem()
+    fs = NtryFilesys()
+    
+    try:
+        ntry_dir = fs.create_empty_filesystem()
+    except FileExistsError as error:
+        typer.secho(f"Error: {error}", fg=typer.colors.RED, err=True)
+        raise typer.Exit(code=1)
 
     print("Initialized nice-try project.")
